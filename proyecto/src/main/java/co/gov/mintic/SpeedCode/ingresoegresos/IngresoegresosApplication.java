807c6aa3cf899d201779a5d22ef209333509c78b
santiago canal <santycanal_06@hotmail.com>
2022-08-28 10:47:41 -0500

    creacion archivo con sprint

---
+++ proyecto/src/main/java/co/gov/mintic/SpeedCode/ingresoegresos/IngresoegresosApplication.java
@@ -0,0 +1,13 @@
+package co.gov.mintic.SpeedCode.ingresoegresos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngresoegresosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngresoegresosApplication.class, args);
+	}
+
+}
---
+++ proyecto/src/test/java/co/gov/mintic/SpeedCode/ingresoegresos/IngresoegresosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gov.mintic.SpeedCode.ingresoegresos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngresoegresosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
